35fd21824232332711059cee2120018d0e8ddf84
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-17 09:05:00 +0000

    Edit CLI methods

--- lib/bahamas_attractions/cli.rb
+++ lib/bahamas_attractions/cli.rb
@@ -1,9 +1,9 @@
 class CLI
 
-  def run
+  def call
       welcome
       Scraper.scrape_attractions
-      print_attractions
+      print_all_attractions
       start_list
   end
   
@@ -15,24 +15,31 @@ class CLI
   
   def start_list
     input = ""
-    
     while input != "exit" do
       puts "Please enter the number located next to the attraction you are interested in: "
       puts "To see the entire list of attractions again, please type 'list'." 
       puts "Otherwise, to exit just type 'exit'!"
       
       input = gets.strip.downcase
-    end
+      #if input is a number and less than that #of attractions but > 0
+       if input = input.to_i && input < Attractions.all.length && input > 0 
+        Scraper.single_attraction_scraper(attractions)             #do i need a test variable here
+        print_single_choice(scraper_single_attraction)
+      elsif input == "list"
+        print_attractions
+      elsif input != "exit"
+        puts "Oh no, it looks like that is not a valid response, please enter a valid option now. "
+      end
+    end    
   end
   
-  def print_attractions
+  def print_all_attractions
     Attractions.all.each.with_index(1) do |att, index|
       puts "#{index}. #{att.name}"
     end
   end
-end
   
-  def print_single_choice(attraction)
+  def print_single_choice(scraper_single_attraction)
     puts "#{attraction.name.upcase}"
     puts ""
     puts "Catagory: #{attraction.catagories}"
@@ -40,12 +47,13 @@ end
     puts "Description:"
     puts "#{attraction.description}"
     puts ""
-    puts "Price: #{attraction.price}"
+    puts "Price: #{attraction.price}" #if no price dont include this--apply to all of these
     puts ""
     puts "Reviews:"
     puts ""
     puts "#{attraction.reviews}"
   end 
+  
   
 end
  
