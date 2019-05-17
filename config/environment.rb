1f1493f2ab51e7dbb13e3799540bc037d75dec35
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-17 01:32:48 +0000

    Re-work file organization

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,6 @@ require 'nokogiri'
 require 'open-uri'
 
 require_relative '../lib/bahamas_attractions/scraper'
-require_relative '../lib/bahamas_attractions/bahamas_attractions'
+require_relative '../lib/bahamas_attractions'
 require_relative '../lib/bahamas_attractions/cli'
 require_relative '../lib/bahamas_attractions/version'
