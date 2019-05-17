bd88be43e88885682dad89a00b71a49daa476a77
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-17 03:33:22 +0000

    edit Attractions

--- lib/bahamas_attractions/attractions.rb
+++ lib/bahamas_attractions/attractions.rb
@@ -2,4 +2,14 @@ class Attractions
   
   attr_accessor :name, :reviews, :catagories
   
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
 end
