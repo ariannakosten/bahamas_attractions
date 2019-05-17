02d5218233511e490f5d0c6e552de39179a40fb1
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-17 01:23:56 +0000

    Environment setup

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+
+require_relative '../lib/bahamas_attractions/scraper'
+require_relative '../lib/bahamas_attractions/bahamas_attractions'
+require_relative '../lib/bahamas_attractions/cli'
+require_relative '../lib/bahamas_attractions/version'
