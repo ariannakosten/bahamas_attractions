1fe4bd2963a01bb933e078bd58403d84df07cb8a
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-17 03:41:04 +0000

    build  Attractions::Initialize

--- lib/bahamas_attractions/attractions.rb
+++ lib/bahamas_attractions/attractions.rb
@@ -1,11 +1,14 @@
 class Attractions
   
-  attr_accessor :name, :reviews, :catagories
+  attr_accessor :name, :reviews, :catagories, :price
   
   @@all = []
   
-  def initialize(name)
+  def initialize(name=nil, reviews=nil, catagories=nil, price=nil)
     @name = name
+    @reviews = reviews
+    @catagories = catagories
+    @price = price
     @@all << self
   end
   
