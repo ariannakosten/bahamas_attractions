8a3fbfa2e329078fd315a2f04138cad370bae7c8
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-18 07:01:31 +0000

    Require gems

--- lib/bahamas_attractions.rb
+++ lib/bahamas_attractions.rb
@@ -8,4 +8,6 @@ require_relative './bahamas_attractions/cli'
 require_relative './bahamas_attractions/attractions'
 require_relative './bahamas_attractions/scraper'
 
-#.. goes back one level - . stay in this level
+#.. goes back one level - . stay in this level (cd)
+# environment file
+# require gems here
